18e60a3ed538e0b76db9826d290252ae5a1d539e
Kyle Baguio <143689709+Hibijuti@users.noreply.github.com>
2025-09-06 21:16:33 +0800

    Final Variation1.c

--- arraylist/Variation1.c
+++ arraylist/Variation1.c
@@ -30,8 +30,14 @@ int main() {
 
     int loc = -1;
     loc = locate(L, 20);
-    printf("%d", loc);
-    
+    printf("Location of 20: %d\n", loc);
+
+    // Test insertSorted
+    L = insertSorted(L, 15);
+    L = insertSorted(L, 5);
+    L = insertSorted(L, 40);
+    display(L);
+
     return 0;
 }
 
@@ -44,6 +50,14 @@ List initialize(List L) {
 }
 
 List insertPos(List L, int pos, int data) {
+    if (L.count >= MAX) {
+        printf("List is full, cannot insert.\n");
+        return L;
+    }
+    if (pos < 0 || pos > L.count) {
+        printf("Invalid position.\n");
+        return L;
+    }
 
     for (int i = L.count; i > pos; i--) {
         L.elem[i] = L.elem[i - 1];
@@ -56,6 +70,11 @@ List insertPos(List L, int pos, int data) {
 }
 
 List deletePos(List L, int position) {
+    if (position < 0 || position >= L.count) {
+        printf("Invalid position.\n");
+        return L;
+    }
+
     for (int i = position; i < L.count - 1; i++) {
         L.elem[i] = L.elem[i + 1];
     }
@@ -64,12 +83,35 @@ List deletePos(List L, int position) {
     return L;
 }
 
-int locate(List L, int data){
-    for(int i = 0; i < MAX; i++){
-        if(L.elem[i] == data){
-            return i;
+int locate(List L, int data) {
+    for (int i = 0; i < L.count; i++) {
+        if (L.elem[i] == data) {
+            return i;  // found
         }
     }
+    return -1; // not found
+}
+
+List insertSorted(List L, int data) {
+    if (L.count >= MAX) {
+        printf("List is full, cannot insert.\n");
+        return L;
+    }
+
+    int pos = 0;
+    while (pos < L.count && L.elem[pos] < data) {
+        pos++;
+    }
+
+    // shift elements to the right
+    for (int i = L.count; i > pos; i--) {
+        L.elem[i] = L.elem[i - 1];
+    }
+
+    L.elem[pos] = data;
+    L.count++;
+
+    return L;
 }
 
 void display(List L) {
