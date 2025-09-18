c5cb20fcbeddea8c092857230af7e187f97d7bec
Kyle Jayverson Baguio <143689709+Hibijuti@users.noreply.github.com>
2025-09-18 17:22:43 +0800

    Update Variation1.c

--- arraylist/Variation1.c
+++ arraylist/Variation1.c
@@ -1,122 +1,94 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define MAX 10
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
 
-typedef struct {
+#define MAX 10 
+
+typedef struct{
     int elem[MAX];
     int count;
-} List;
-
+}List;
 List initialize(List L);
-List insertPos(List L, int pos, int data);
+List insertPos(List L, int data, int position);
 List deletePos(List L, int position);
 int locate(List L, int data);
 List insertSorted(List L, int data);
 void display(List L);
 
-int main() {
-    List L;
-    L.count = 0;
-    L = initialize(L);
-
-    L = insertPos(L, 0, 10);
-    L = insertPos(L, 1, 20);
-    L = insertPos(L, 2, 30);
-
+int main(){
+    List L = initialize(L);
+    L = insertPos(L, 5, 0);
+    L = insertPos(L, 10, 1);
+    L = insertPos(L, 15, 2);
+    L = insertPos(L, 20, 3);
     display(L);
-
     L = deletePos(L, 1);
+    
+    int located = locate(L, 15);
+    printf("%d\n", located);
+    
+    L = insertSorted(L, 30);
     display(L);
-
-    int loc = -1;
-    loc = locate(L, 20);
-    printf("Location of 20: %d\n", loc);
-
-    // Test insertSorted
-    L = insertSorted(L, 15);
-    L = insertSorted(L, 5);
-    L = insertSorted(L, 40);
-    display(L);
-
-    return 0;
 }
 
-List initialize(List L) {
-    for (int i = 0; i < MAX; i++) {
-        L.elem[i] = -1;
-    }
+List initialize(List L){
     L.count = 0;
     return L;
 }
 
-List insertPos(List L, int pos, int data) {
-    if (L.count >= MAX) {
-        printf("List is full, cannot insert.\n");
+List insertPos(List L, int data, int position){
+    if(position > L.count || L.count >= MAX){
+        printf("Error1");
         return L;
     }
-    if (pos < 0 || pos > L.count) {
-        printf("Invalid position.\n");
-        return L;
+    
+    for(int i = L.count - 1; i >= position; i--){
+        L.elem[i + 1] = L.elem[i];
     }
-
-    for (int i = L.count; i > pos; i--) {
-        L.elem[i] = L.elem[i - 1];
-    }
-
-    L.elem[pos] = data;
+    L.elem[position] = data;
     L.count++;
-
     return L;
 }
 
-List deletePos(List L, int position) {
-    if (position < 0 || position >= L.count) {
-        printf("Invalid position.\n");
+List deletePos(List L, int position){
+    if(position >= L.count){
+        printf("Error2");
         return L;
     }
-
-    for (int i = position; i < L.count - 1; i++) {
+    for(int i = position; i < L.count - 1; i++){
         L.elem[i] = L.elem[i + 1];
     }
+    
     L.count--;
-
     return L;
 }
 
-int locate(List L, int data) {
-    for (int i = 0; i < L.count; i++) {
-        if (L.elem[i] == data) {
-            return i;  // found
+int locate(List L, int data){
+    for(int i = 0; i < L.count; i++){
+        if(data == L.elem[i]){
+            return i;
         }
     }
-    return -1; // not found
+    return -1;
 }
 
-List insertSorted(List L, int data) {
-    if (L.count >= MAX) {
-        printf("List is full, cannot insert.\n");
+List insertSorted(List L, int data){
+    if(L.count == MAX){
+        printf("FULL");
         return L;
     }
-
-    int pos = 0;
-    while (pos < L.count && L.elem[pos] < data) {
-        pos++;
+    int i;
+    for(i = L.count - 1; i >= 0 && L.elem[i] > data; i--){
+        L.elem[i + 1] = L.elem[i];
     }
-
-    // shift elements to the right
-    for (int i = L.count; i > pos; i--) {
-        L.elem[i] = L.elem[i - 1];
-    }
-
-    L.elem[pos] = data;
+    L.elem[i + 1] = data;
     L.count++;
-
     return L;
 }
 
-void display(List L) {
-    for (int i = 0; i < L.count; i++) {
-        printf("%d ", L.elem[i]);
+void display(List L){
+    for(int i = 0; i < L.count; i++){
+        printf("%d\n", L.elem[i]);
     }
-    printf("\n");
 }
+
